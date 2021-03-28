c2a72c49d511fba8d5cf04e2ec34a952e652a559
Kaine <emailkaine@gmail.com>
2021-03-28 05:58:17 +0100

    more notes, dir clean script, gitignore additions

---
+++ notes/clear-tex.sh
@@ -0,0 +1,3 @@
+#!/user/bin/env bash
+
+rm notes.aux && rm notes.bcf && rm notes.log && rm notes.out && rm notes.pdf && rm notes.run.xml && rm texput.log && rm notes.tex.bb && rm notes.tex.blg rm && BaaS-analysis.bib.blg rm && notes.tex.blg rm && notes.dvi rm && notes.tex.bbl rm && BaaS-analysis.bib.bbl
